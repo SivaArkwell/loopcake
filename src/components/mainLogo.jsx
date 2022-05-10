3aaeefc983c00b28d10d42ed503147ddbed49540
Andrew Arkwell <andrew@sivacreative.com>
2022-05-09 23:51:19 -0400

    Testing component

---
+++ src/components/mainLogo.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { Parallax } from "@react-spring/parallax"
+import { Themed } from "theme-ui"
+
+const MainLogo = () => (
+    <div>
+        <img src="https://loopcake.com/loopcakev1logo.gif"/>
+    </div>
+)
+
+export default MainLogo
